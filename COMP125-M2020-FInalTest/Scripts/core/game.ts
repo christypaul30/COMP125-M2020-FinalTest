1933618d8b83f00237acfc8225fae318eca01238
Christy Paul <christypaul59@gmail.com>
2020-08-18 20:48:32 -0400

    final changes

--- COMP125-M2020-FInalTest/Scripts/core/game.ts
+++ COMP125-M2020-FInalTest/Scripts/core/game.ts
@@ -18,10 +18,10 @@ function rollTheDice() {
         let randomNumber1 = Math.floor(Math.random() * 6) + 1; 
         let randomNumber2 = Math.floor(Math.random() * 6) + 1; 
 
-        document.querySelector(".img1").setAttribute("src", 
+        document.querySelector(".1").setAttribute("src", 
             "dice" + randomNumber1 + ".png"); 
 
-        document.querySelector(".img2").setAttribute("src", 
+        document.querySelector(".2").setAttribute("src", 
             "dice" + randomNumber2 + ".png"); 
 
         if (randomNumber1 === randomNumber2) { 
